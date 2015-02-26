90eb28de963ea4e5a760fcdda8f7805013ebf784
nnegrey <noah.negrey@gmail.com>
2015-02-25 20:09:06 -0800

    Updated setup file with intial ideas

---
+++ setup/setup.ino
@@ -0,0 +1,56 @@
+/** Main Functions
+ * 1) Line Tracing
+ * 2) Movement
+ * 3) Off line movement (SOS)
+ * 4) Pickup Rings
+ * 5) Place Rings
+ * 6) Center Rings
+ * 7) Music
+ * Center Peg - Last 5 minutes 
+ */
+
+void setup() {
+
+}
+
+
+void loop() {
+  
+}
+
+/** Initial Start Movement
+ * -Get to the middle line
+ * -Rotate toward the horizontal rings (to the right)
+ */
+void initialMovement() {
+  
+}
+
+
+/** Pickup Rings Movement
+ * -Move to the horizontal line (before the horizontal rings)
+ * -Confirm that the arm is at a height of 6 inches. (create lowerTheArm()
+ * -Move toward the wall
+ * -pickupRings()
+ * -placeRings()
+ */
+void getRingsMovement() {
+  
+}
+
+
+/** Move To The Middle With The Rings and Place Them
+ * -Full reverse until at the center line
+ * -Rotate 180 degrees
+ * -Moe to the horizontal line (before the vertical rings)
+ * -raiseTheArm()
+ * -Move toward the wall
+ * -placeRings()
+ * -returnToMidLine
+ * -rotate 180 degrees
+ * -getRingsMovement()
+ */
+void placeRings() {
+  
+}
+
